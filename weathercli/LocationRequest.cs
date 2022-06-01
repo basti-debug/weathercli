b3fc60f69187f766e1109c02890122fa3ead5102
Joel05 <joel.rupp05@gmail.com>
2022-06-01 08:40:20 +0200

    added Location

---
+++ weathercli/LocationRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Device.Location;
+public static class LocationRequest
+{
+    public static double Latitude;
+    public static double Longitude;
+    public static void Start()
+        {
+            GeoCoordinateWatcher watcher;
+            watcher = new GeoCoordinateWatcher();
+
+            watcher.PositionChanged += (sender, e) =>
+            {
+                var coordinate = e.Position.Location;
+                Latitude = coordinate.Latitude;
+                Longitude = coordinate.Longitude;
+                Console.WriteLine("Position has changed: Lat:" + Latitude + " Lon:" + Longitude);
+            };
+
+            // Begin listening for location updates.
+            watcher.Start();
+        }
+    public static double GPSLatitude()
+    {
+        return Latitude;
+    }
+    public static double GPSLongitude()
+    {
+        return Longitude;
+    }
+    
+}
--- weathercli/Program.cs
+++ weathercli/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Device.Location;
 
 namespace weathercli
 {
@@ -10,12 +11,21 @@ namespace weathercli
     {
         static void Main(string[] args)
         {
-            public static DateTime unixconv(int unix)
+            //public static DateTime unixconv(int unix)
+            //{
+            //    DateTime dt = new DateTime(1970,1,1,0,0,0,0,DateTime.UtcNow);
+            //    dt = dt.AddSeconds( unix ).ToLocalTime();
+            //    return dt;
+            //}
+            
+            LocationRequest.Start();
+            while (LocationRequest.Latitude == 0)
             {
-                DateTime dt = new DateTime(1970,1,1,0,0,0,0,DateTime.UtcNow);
-                dt = dt.AddSeconds( unix ).ToLocalTime();
-                return dt;
+                ;
             }
+            Console.WriteLine(LocationRequest.Latitude);
+            Console.ReadKey();
         }
     }
+    
 }
