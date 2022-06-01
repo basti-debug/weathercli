a9dba3b0f1324d72f5f89e65923e4cc08528b932
basti.debug <basti.debug@gmail.com>
2022-06-01 08:56:15 +0200

    frist notification

--- weathercli/Program.cs
+++ weathercli/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Toolkit.Uwp.Notifications;      
 
 namespace weathercli
 {
@@ -10,12 +6,14 @@ namespace weathercli
     {
         static void Main(string[] args)
         {
-            public static DateTime unixconv(int unix)
+            /*public DateTime unixconv(int unix)
             {
-                DateTime dt = new DateTime(1970,1,1,0,0,0,0,DateTime.UtcNow);
-                dt = dt.AddSeconds( unix ).ToLocalTime();
+                DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Local);
+                dt = dt.AddSeconds(unix).ToLocalTime();
                 return dt;
-            }
+            }*/
+            new ToastContentBuilder().AddText("test").Show();
+
         }
     }
 }
